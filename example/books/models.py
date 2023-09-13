85a77a47b11bfd4c6bcd2b2e7fb47efa1fa219cc
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-09-13 18:57:29 -0300

    feat(example): add book cover

--- example/books/models.py
+++ example/books/models.py
@@ -4,5 +4,6 @@ from .faker import BookFaker
 
 @faker_fields(**BookFaker)
 class Book(models.Model):
+    cover = models.ImageField(blank=True, null=True, default=None)
     title = models.CharField(max_length=160)
     description = models.TextField()
