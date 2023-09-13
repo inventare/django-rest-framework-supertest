a54075ed7d515b02ef7a4c58725c7201669009cc
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-09-13 19:04:38 -0300

    feat(example): write an test for images

--- example/books/faker.py
+++ example/books/faker.py
@@ -4,7 +4,19 @@ def faker_name(fake):
 def faker_description(fake):
     return fake.sentence(30)
 
+def faker_cover(fake):
+    from io import BytesIO
+    from PIL import Image
+    from django.core.files.base import ContentFile
+
+    image_file = BytesIO()
+    image = Image.new("RGBA", size=(50, 50), color=(256, 0, 0))
+    image.save(image_file, "png")
+    image_file.seek(0)
+    return ContentFile(image_file.read(), "test.png")
+
 BookFaker = {
     'title': faker_name,
     'description': faker_description,
+    'cover': faker_cover,
 }
