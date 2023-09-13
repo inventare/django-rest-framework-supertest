c6a1c8e3f09b901b6106c2d0ff6b30bd437be1e3
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-09-13 19:22:08 -0300

    refactor: move image creation to faker provider

--- example/books/faker.py
+++ example/books/faker.py
@@ -5,15 +5,7 @@ def faker_description(fake):
     return fake.sentence(30)
 
 def faker_cover(fake):
-    from io import BytesIO
-    from PIL import Image
-    from django.core.files.base import ContentFile
-
-    image_file = BytesIO()
-    image = Image.new("RGBA", size=(50, 50), color=(256, 0, 0))
-    image.save(image_file, "png")
-    image_file.seek(0)
-    return ContentFile(image_file.read(), "test.png")
+    return fake.image(width=256, height=256)
 
 BookFaker = {
     'title': faker_name,
