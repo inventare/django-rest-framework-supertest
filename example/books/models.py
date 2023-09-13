cc8cdf3c9d1b8b641a1ad41f568c6fa805505f87
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-09-13 17:50:28 -0300

    refactor: remove create_faker mixin and move to an function (#12)

--- example/books/models.py
+++ example/books/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from rest_framework_supertest.models.decorators import faker_field
-from rest_framework_supertest.models.base import FakerModelMixin
-
 
 @faker_field('title', lambda fake : fake.name())
-class Book(FakerModelMixin, models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=160)
--- rest_framework_supertest/models/base.py
+++ rest_framework_supertest/models/base.py
@@ -1,25 +1,23 @@
 from typing import Optional
 from faker import Faker
 
-class FakerModelMixin:
-    @classmethod
-    def create_faker(cls, data: Optional[dict] = None, save = True):
-        fake = Faker('pt_BR')
+def create_faker(model_class, data: Optional[dict] = None, save = True):
+    fake = Faker('pt_BR')
 
-        faker_fields = cls.faker_fields
-        fields = faker_fields.keys()
-        create_dict = {}
-        for field in fields:
-            create_dict[field] = faker_fields[field](fake)
+    faker_fields = model_class.faker_fields
+    fields = faker_fields.keys()
+    create_dict = {}
+    for field in fields:
+        create_dict[field] = faker_fields[field](fake)
 
-        if not data:
-            data = {}
-            
-        create_dict = {
-            **create_dict,
-            **data,
-        }
+    if not data:
+        data = {}
+        
+    create_dict = {
+        **create_dict,
+        **data,
+    }
 
-        if save:
-            return cls.objects.create(**create_dict)
-        return cls(**create_dict)
+    if save:
+        return model_class.objects.create(**create_dict)
+    return model_class(**create_dict)
