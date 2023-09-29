df2f6e186b91a2dcdaa5aa99c7172cbfa8ac2f89
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-09-29 14:30:43 -0300

    feat: add get_validation_response utils

---
+++ rest_framework_supertest/tests/utils/__init__.py
@@ -0,0 +1,3 @@
+from .validation_response import get_validation_response
+
+__all__ = ['get_validation_response']
---
+++ rest_framework_supertest/tests/utils/validation_response.py
@@ -0,0 +1,21 @@
+import json
+from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.serializers import ValidationError
+
+def get_validation_response(self, serializer):
+    data = '{}'
+    try:
+        serializer.is_valid(raise_exception=True)
+    except ValidationError as e:
+        data = json.dumps(e.detail)
+    
+    resp = HttpResponse(
+        data,
+        status=status.HTTP_400_BAD_REQUEST,
+        headers={
+            'Content-Type': 'application/json'
+        }
+    )
+    resp.json = lambda: json.loads(resp.content.decode())
+    return resp
