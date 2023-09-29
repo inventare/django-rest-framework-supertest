d8bc65657b13ac182e43f3cd114ec6a36eda5ee9
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-09-28 22:30:15 -0300

    refactor: move mixins to an folder and split files

--- rest_framework_supertest/mixins/authentication.py
+++ rest_framework_supertest/mixins/authentication.py
@@ -1,65 +1,7 @@
-from typing import List, Optional
-import json
-from django.http import HttpResponse
+from typing import Optional
 from django.db.models import Model
 from django.utils.module_loading import import_string
-from rest_framework_supertest.utils.exceptions import APIExceptionsUtils
 from rest_framework_supertest.authentication import AuthenticationBase
-
-
-class AssertAPIResponseMixin:
-    def assertResponseHeaders(self, response: HttpResponse, headers):
-        for header in headers.keys():
-            value = headers.get(header)
-            self.assertEquals(response.headers.get(header), value)
-    
-    def assertResponseJson(self, response: HttpResponse, data: dict):
-        return self.assertEquals(json.dumps(data), json.dumps(response.json()))
-
-
-class AssertAPIExceptionMixin:
-    def assertAPIException(self, response: HttpResponse, exception):
-        if not hasattr(self, 'assertResponseJson'):
-            raise AttributeError(
-                "To use assertAPIException method, assertResponseJson must be present in the TestCase. "
-                "Extends AssertAPIResponseMixin on your TestCase"
-            )
-        if not hasattr(self, 'assertResponseHeaders'):
-            raise AttributeError(
-                "To use assertAPIException method, assertResponseHeaders must be present in the TestCase. "
-                "Extends AssertAPIResponseMixin on your TestCase"
-            )
-
-        handler = APIExceptionsUtils(response, exception)
-        data, status, headers = handler.exception_handler()
-
-        self.assertEquals(response.status_code, status)
-        self.assertResponseJson(response, data)
-        self.assertResponseHeaders(response, headers)
-
-    def assertOneOfAPIExceptions(self, response: HttpResponse, exceptions: List[any]):
-        found_one = False
-        for exception in exceptions:
-            handler = APIExceptionsUtils(response, exception)
-            data, status, headers = handler.exception_handler()
-
-            if response.status_code != status:
-                continue
-            if json.dumps(data) != json.dumps(response.json()):
-                continue
-
-            found_one = True
-            for header in headers.keys():
-                value = headers.get(header)
-
-                if response.headers.get(header) != value:
-                    found_one = False
-
-            if found_one:
-                break
-
-        if not found_one:
-            self.fail('None of the APIException\'s is raised to the response.')
 
 class AssertAuthenticationMixin:
     authentication_class = None
@@ -119,8 +61,3 @@ class AssertAuthenticationMixin:
         
         exceptions = self.authentication.authentication_failed_exceptions
         self.assertOneOfAPIExceptions(response, exceptions)
-
-__all__ = [
-    'AssertAPIExceptionMixin',
-    'AssertAuthenticationMixin',
-]
