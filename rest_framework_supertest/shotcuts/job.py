1039f9e9b66892d3576ac9dc548757f62a272a15
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-10-22 20:53:14 -0300

    feat: add shotcuts for job (#51)

---
+++ rest_framework_supertest/shotcuts/job.py
@@ -0,0 +1,9 @@
+from ._utils import unique
+
+def job_name(fake):
+    """Generate a job name."""
+    return fake.job()
+
+def unique_job_name(fake):
+    """Generate a unique job name."""
+    return unique(fake, job_name)
