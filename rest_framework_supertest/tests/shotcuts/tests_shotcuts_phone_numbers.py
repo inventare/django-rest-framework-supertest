42afc3851095955a5fb09c7755b7ddef80691a77
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-10-26 20:12:05 -0300

    feat: add shotcuts for phone number

---
+++ rest_framework_supertest/tests/shotcuts/tests_shotcuts_phone_numbers.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from rest_framework_supertest.shotcuts import phone_numbers
+from .base import FakerMockMixin
+
+class PhoneNumbersShotcuts(FakerMockMixin, TestCase):
+    def test_phone_number(self):
+        self.exec_test(['phone_number'], phone_numbers, 'phone_number')
+
+    def test_unique_phone_number(self):
+        self.exec_test(['unique', 'phone_number'], phone_numbers, 'unique_phone_number')
+
+    def test_country_calling_code(self):
+        self.exec_test(['country_calling_code'], phone_numbers, 'country_calling_code')
+
+    def test_unique_country_calling_code(self):
+        self.exec_test(['unique', 'country_calling_code'], phone_numbers, 'unique_country_calling_code')
