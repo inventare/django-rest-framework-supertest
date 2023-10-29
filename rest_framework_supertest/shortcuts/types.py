a77a33fd260d985afe155529546563e8d7e25aec
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-10-28 23:06:41 -0300

    fix: updating some linter rules

--- rest_framework_supertest/shortcuts/types.py
+++ rest_framework_supertest/shortcuts/types.py
@@ -1,5 +1,7 @@
 from typing import Union
 import datetime
 
+# TODO: ListOfInt: TypeAlias = list[int]
+
 DateTimeOptionalArg = Union[datetime.date, datetime.datetime, datetime.timedelta, str, int, None]
 DateTimeArg = Union[datetime.date, datetime.datetime, datetime.timedelta, str, int]
