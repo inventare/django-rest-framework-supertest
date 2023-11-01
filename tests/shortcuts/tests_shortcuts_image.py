f39e5b5d518bae719a375fb8f2660e643947c1de
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-11-01 07:57:04 -0300

    test: write test for unseted file name on image shortcut

--- tests/shortcuts/tests_shortcuts_image.py
+++ tests/shortcuts/tests_shortcuts_image.py
@@ -1,3 +1,5 @@
+from unittest.mock import MagicMock, patch
+
 from faker import Faker
 
 from rest_framework_supertest.shortcuts import images
@@ -26,6 +28,27 @@ class ImagesShortcutsTests(ShortcutTestCase):
         mock.assert_called_once_with(size=(width, height), image_format=image_format)
         self.assertIsNotNone(output)
         self.assertEqual(output.name, image_name)
+
+    @patch('rest_framework_supertest.shortcuts.images.uuid')
+    def test_image_without_name(self, mock: MagicMock) -> None:
+        file_name = "file_name"
+        width = 512
+        height = 512
+        image_format = "tiff"
+        mock.uuid4 = MagicMock()
+        mock.uuid4.return_value = file_name
+        fake = Faker()
+        output = images.image(
+            fake,
+            width=width,
+            height=height,
+            image_format=image_format,
+        )
+        expected_name = f"{file_name}.{image_format}"
+
+        mock.uuid4.assert_called_once()
+        self.assertIsNotNone(output)
+        self.assertEqual(output.name, expected_name)
 
     def test_image_store_to_field(self) -> None:
         width = 512
