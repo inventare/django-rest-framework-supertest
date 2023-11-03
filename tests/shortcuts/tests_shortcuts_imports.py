dc3cdc5501e4ec06cbd1655d32f75d57ac1448cd
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-11-03 13:29:34 -0300

    test: write test to grant re-export

--- tests/shortcuts/tests_shortcuts_imports.py
+++ tests/shortcuts/tests_shortcuts_imports.py
@@ -1,3 +1,4 @@
+import importlib
 from types import FunctionType, ModuleType
 from typing import List
 
@@ -19,11 +20,24 @@ class ShortcutsImportTests(TestCase):
             modules = [*modules, item]
         return modules
 
+    def _get_functions(self) -> List[str]:
+        items = dir(shortcuts)
+        functions = []
+        for item in items:
+            if item.startswith(("__", "_")):
+                continue
+            item_prop = getattr(shortcuts, item)
+            if not isinstance(item_prop, FunctionType):
+                continue
+            functions = [*functions, item]
+        return functions
 
     def setUp(self) -> None:
         self.modules = self._get_modules()
+        self.functions = self._get_functions()
 
     def test_reimports(self) -> None:
+        """Grant if all exported from shortcuts is intern of it's modules."""
         items = dir(shortcuts)
         for item in items:
             if item in self.modules:
@@ -34,7 +48,24 @@ class ShortcutsImportTests(TestCase):
             module_name = item_prop.__module__
             self.assertTrue(module_name.startswith('rest_framework_supertest.shortcuts.'))
             module_name = module_name.replace('rest_framework_supertest.shortcuts.', '')
-            self.assertTrue(module_name in self.modules)
+            self.assertIn(module_name, self.modules)
 
+    def test_reexport(self) -> None:
+        """Grant if all exported from intern modules is exported from __init__."""
+        items = self.modules
+        for item in items:
+            module_name = f"rest_framework_supertest.shortcuts.{item}"
+            module = importlib.import_module(module_name)
+            subitems = dir(module)
+
+            for sub in subitems:
+                if sub.startswith(("__", "_")):
+                    continue
+                if sub == 'unique':
+                    continue
+                sub_prop = getattr(module, sub)
+                if not isinstance(sub_prop, FunctionType):
+                    continue
+                self.assertIn(sub, self.functions)
 
 __all__ = []
